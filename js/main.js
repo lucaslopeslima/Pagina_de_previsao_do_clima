5f38b76a113e29b14f85355f0f2332c5cbc2dec4
Lucas L. Lima <lucaslopes.developer@gmail.com>
2022-11-14 23:21:38 -0300

    HTML
    html prototype

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m
-let cityName = 'Teresina'
+let cityName = 'London'
 let geo = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},&limit=1&appid=2bcb7be76a6327aeef106952bdf73c70`
 
 fetch(geo)
@@ -53,17 +53,17 @@ fetch(url)
 /*
 WMO Weather interpretation codes (WW)
 Code	Description
-0	        Clear sky
-1, 2, 3	    Mainly clear, partly cloudy, and overcast
-45, 48	    Fog and depositing rime fog
-51, 53, 55	Drizzle: Light, moderate, and dense intensity
-56, 57	    Freezing Drizzle: Light and dense intensity
-61, 63, 65	Rain: Slight, moderate and heavy intensity
-66, 67	    Freezing Rain: Light and heavy intensity
+0	        Clear sky <i class="fa-light fa-sun"></i>
+1, 2, 3	    Mainly clear, partly cloudy, and overcast <i class="fa-regular fa-sun-cloud"></i>
+45, 48	    Fog and depositing rime fog <i class="fa-regular fa-smog"></i>
+51, 53, 55	Drizzle: Light, moderate, and dense intensity <i class="fa-duotone fa-raindrops"></i> <i class="fa-solid fa-cloud-drizzle"></i>
+56, 57	    Freezing Drizzle: Light and dense intensity <i class="fa-duotone fa-temperature-snow"></i>
+61, 63, 65	Rain: Slight, moderate and heavy intensity <i class="fa-duotone fa-umbrella"></i>
+66, 67	    Freezing Rain: Light and heavy intensity 
 71, 73, 75	Snow fall: Slight, moderate, and heavy intensity
 77	        Snow grains
-80, 81, 82	Rain showers: Slight, moderate, and violent
-85, 86	    Snow showers slight and heavy
-95 *	    Thunderstorm: Slight or moderate
-96, 99 *	Thunderstorm with slight and heavy hail
+80, 81, 82	Rain showers: Slight, moderate, and violent <i class="fa-solid fa-cloud-showers-heavy"></i>
+85, 86	    Snow showers slight and heavy <i class="fa-solid fa-face-icicles"></i>
+95 *	    Thunderstorm: Slight or moderate <i class="fa-duotone fa-cloud-bolt"></i>
+96, 99 *	Thunderstorm with slight and heavy hail <i class="fa-duotone fa-cloud-bolt"></i>
 */
